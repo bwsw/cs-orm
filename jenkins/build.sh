36f21bda4843f1ec5f48dfdfbfd555df6a790d59
Vladislav Medvedev <medvedev_vv@bw-sw.com>
2017-12-05 11:48:54 +0700

    CSEN-11 sbt command for scala style check was added into build.sh

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -5,3 +5,11 @@ echo "----------------- Unit tests ----------------"
 echo "---------------------------------------------"
 
 sbt clean coverage test coverageReport
+
+echo "---------------------------------------------"
+echo "-------------- Scalastyle checks ------------"
+echo "---------------------------------------------"
+
+sbt scalastyle
+
+sbt test:scalastyle
